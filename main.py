f1e396a8837103da88ad45450b76b8bb47794b48
abatima <renat.kulalov.work@gmail.com>
2026-02-12 13:30:20 +0100

    added checkmark counter and replaced math.floor with //

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from tkinter import *
-import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 0.1
-SHORT_BREAK_MIN = 0.1
-LONG_BREAK_MIN = 0.1
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
@@ -37,7 +36,7 @@ def start_timer():
 
 def count_down(count):
 
-    count_min = math.floor(count / 60)
+    count_min = count // 60
     count_sec = count % 60
 
     if count_sec < 10:
@@ -48,6 +47,8 @@ def count_down(count):
         window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        work_sessions_completed = reps//2
+        checkmark_label.config(text="✔"* work_sessions_completed)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -58,7 +59,7 @@ window.config(padx=100, pady=50, bg=YELLOW)
 timer_label = Label(text = "Timer", font=(FONT_NAME, 30, "bold"), fg=GREEN, bg=YELLOW)
 timer_label.grid(column=1,row=0)
 
-checkmark_label = Label(text = "✔", font=(FONT_NAME, 20, "bold"), fg=GREEN, bg=YELLOW)
+checkmark_label = Label(fg=GREEN, bg=YELLOW)
 checkmark_label.grid(column=1,row=3)
 
 button = Button(text="Start", highlightthickness=0, command=start_timer)
@@ -67,7 +68,7 @@ button.grid(column=0,row=2)
 button = Button(text="Reset", highlightthickness=0)
 button.grid(column=2,row=2)
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas = Canvas(width=220, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100,112, image= tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
